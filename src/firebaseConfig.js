a3afd5d8bf709f0abdbb79a0fc02b1de1fb275ca
lucasleirbag <citric5828@tutamail.com>
2024-08-13 01:55:29 -0300

    feat: Add Firebase Analytics to project
    This commit adds the necessary code to initialize Firebase Analytics in the project by importing the `getAnalytics` function from the `firebase/analytics` module and calling it with the `app` object. This will enable tracking and analysis of user behavior and interactions within the application.

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -11,7 +12,9 @@ const firebaseConfig = {
     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const analytics = getAnalytics(app);
 
 export { db };

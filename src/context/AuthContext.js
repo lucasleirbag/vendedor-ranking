4e970218dc671108f8b7a4a10170f09e64aa9c39
lucas.ribeiro <lucas.ribeiro@cebraspe.org.br>
2024-08-13 12:06:26 -0300

    feat: Refactor code in AuthContext.js and add localStorage support
    This commit refactors the code in AuthContext.js to include the use of localStorage for storing user data. It also adds a useEffect hook to redirect the user to the login page if there is no user data present. These changes improve the user authentication functionality and provide a better user experience.

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,4 @@
-// src/context/AuthContext.js
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const AuthContext = createContext();
@@ -7,34 +6,38 @@ const AuthContext = createContext();
 export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState(() => {
+        const storedUser = localStorage.getItem('user');
+        return storedUser ? JSON.parse(storedUser) : null;
+    });
     const navigate = useNavigate();
 
     const login = (inputUsername, inputPassword) => {
         const username = process.env.REACT_APP_ADMIN_USERNAME;
         const password = process.env.REACT_APP_ADMIN_PASSWORD;
-    
-        if (!username || !password) {
-            console.error("Variáveis de ambiente não estão carregadas corretamente.");
-        } else {
-            console.log("Usuário e senha das variáveis de ambiente foram carregados com sucesso.");
-        }
-    
+
         if (inputUsername === username && inputPassword === password) {
-            console.log("Login bem-sucedido");
-            setUser({ displayName: inputUsername });
-            navigate('/');
+            const loggedInUser = { displayName: inputUsername };
+            setUser(loggedInUser);
+            localStorage.setItem('user', JSON.stringify(loggedInUser)); // Armazena o usuário no localStorage
+            navigate('/'); // Redirecionar para a home após login bem-sucedido
         } else {
             console.error("Credenciais inválidas.");
             alert("Credenciais inválidas.");
         }
-    };    
+    };
 
     const logout = () => {
-        console.log("Usuário fez logout");
         setUser(null);
+        localStorage.removeItem('user'); // Remove o usuário do localStorage
         navigate('/login'); // Redirecionar para a página de login após logout
     };
+
+    useEffect(() => {
+        if (!user) {
+            navigate('/login');
+        }
+    }, [user, navigate]);
 
     return (
         <AuthContext.Provider value={{ user, login, logout }}>

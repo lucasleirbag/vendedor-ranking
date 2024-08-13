0753a149fd5046eacd250432932f54219ffda5e6
lucas.ribeiro <lucas.ribeiro@cebraspe.org.br>
2024-08-13 09:30:06 -0300

    feat: Update admin login credentials in AuthContext.js
    The code changes in AuthContext.js update the admin login credentials from the environment variables `REACT_APP_PROD_USER` and `REACT_APP_PROD_PASSWORD` to `REACT_APP_ADMIN_USERNAME` and `REACT_APP_ADMIN_PASSWORD` respectively. This ensures that the correct admin credentials are used for logging in.

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,19 +1,6 @@
 // src/context/AuthContext.js
 import React, { createContext, useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { initializeApp } from 'firebase/app';
-
-const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-};
-
-const app = initializeApp(firebaseConfig);
 
 const AuthContext = createContext();
 
@@ -24,8 +11,8 @@ export const AuthProvider = ({ children }) => {
     const navigate = useNavigate();
 
     const login = (inputUsername, inputPassword) => {
-        const username = process.env.REACT_APP_PROD_USER;
-        const password = process.env.REACT_APP_PROD_PASSWORD;
+        const username = process.env.REACT_APP_ADMIN_USERNAME;
+        const password = process.env.REACT_APP_ADMIN_PASSWORD;
 
         if (inputUsername === username && inputPassword === password) {
             setUser({ displayName: inputUsername });

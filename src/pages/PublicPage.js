903e636c1c6c9a253f3088af5cfcf4670d2ee409
lucas.ribeiro <lucas.ribeiro@cebraspe.org.br>
2024-08-13 17:42:55 -0300

    Atualizar barra de pesquisa para adicionar funcionalidade de fechar e abrir

--- src/pages/PublicPage.js
+++ src/pages/PublicPage.js
@@ -1,17 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import VendedorList from '../components/VendedorList';
 import SearchBar from '../components/SearchBar';
-import { Box, Container, Typography, IconButton } from '@mui/material';
+import { Box, Container, Typography, IconButton, Collapse } from '@mui/material';
 import { getVendedores } from '../services/firestoreService';
 import CompradoresModal from '../components/CompradoresModal';
 import ShareIcon from '@mui/icons-material/Share';
+import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
 import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
 import Signature from '../components/Signature';
+import '../styles/styles.css';
 
 function PublicPage() {
     const [vendedores, setVendedores] = useState([]);
     const [selectedVendedor, setSelectedVendedor] = useState(null);
     const [searchQuery, setSearchQuery] = useState('');
+    const [showSearch, setShowSearch] = useState(false);
 
     useEffect(() => {
         async function fetchVendedores() {
@@ -57,41 +61,57 @@ function PublicPage() {
     };
 
     return (
-        <Container maxWidth="sm" sx={{ padding: 2, position: 'relative', minHeight: '100vh' }}>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: 2 }}>
-                <SearchBar onSearch={setSearchQuery} />
-                <IconButton onClick={handleCopyLink} color="primary" aria-label="compartilhar link">
-                    <ShareIcon />
-                </IconButton>
-            </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'center', marginY: 2 }}>
-                {congregacaoRankingSorted.map(([congregacao, total], index) => (
-                    <Box key={congregacao} sx={{ textAlign: 'center', marginX: 2 }}>
-                        <EmojiEventsIcon style={{ color: getTrophyColor(index), fontSize: '3rem' }} />
-                        <Typography variant="h6">
-                            {index + 1}° {congregacao}
-                        </Typography>
-                        <Typography variant="subtitle1">
-                            {total} vendas
-                        </Typography>
+        <div className="parallax-container">
+            <div className="fixed-header">
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '16px' }}>
+                    <IconButton onClick={() => setShowSearch(!showSearch)} color="primary" aria-label="abrir barra de pesquisa">
+                        {showSearch ? <CloseIcon /> : <SearchIcon />}
+                    </IconButton>
+                    <IconButton onClick={handleCopyLink} color="primary" aria-label="compartilhar link">
+                        <ShareIcon />
+                    </IconButton>
+                </Box>
+
+                <Collapse in={showSearch}>
+                    <SearchBar onSearch={setSearchQuery} />
+                </Collapse>
+            </div>
+
+            <Container maxWidth="sm" sx={{ padding: 2, position: 'relative', minHeight: '100vh', marginTop: '80px' }}>
+                <Collapse in={!showSearch}>
+                    <Box sx={{ display: 'flex', justifyContent: 'center', marginY: 2 }}>
+                        {congregacaoRankingSorted.map(([congregacao, total], index) => (
+                            <Box key={congregacao} sx={{ textAlign: 'center', marginX: 2 }}>
+                                <EmojiEventsIcon style={{ color: getTrophyColor(index), fontSize: '3rem' }} />
+                                <Typography variant="h6">
+                                    {index + 1}° {congregacao}
+                                </Typography>
+                                <Typography variant="subtitle1">
+                                    {total} vendas
+                                </Typography>
+                            </Box>
+                        ))}
                     </Box>
-                ))}
-            </Box>
-            <VendedorList
-                vendedores={filteredVendedores}
-                onUpdateQuantidade={null}
-                onDelete={null}
-                onShowCompradores={handleShowCompradores}
-            />
-            {selectedVendedor && (
-                <CompradoresModal 
-                    open={Boolean(selectedVendedor)} 
-                    onClose={() => setSelectedVendedor(null)} 
-                    vendedor={selectedVendedor} 
+                </Collapse>
+
+                <VendedorList
+                    vendedores={filteredVendedores}
+                    onUpdateQuantidade={null}
+                    onDelete={null}
+                    onShowCompradores={handleShowCompradores}
                 />
-            )}
-            <Signature />
-        </Container>
+
+                {selectedVendedor && (
+                    <CompradoresModal 
+                        open={Boolean(selectedVendedor)} 
+                        onClose={() => setSelectedVendedor(null)} 
+                        vendedor={selectedVendedor} 
+                    />
+                )}
+
+                <Signature />
+            </Container>
+        </div>
     );
 }
 

300320eadc3e85da53138918f673a58cd78742e8
lucas.ribeiro <lucas.ribeiro@cebraspe.org.br>
2024-08-13 15:14:51 -0300

    Atualizar serviço de banco de dados para Firestore em UpdateQuantidadeModal.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -56,9 +56,8 @@ function HomePage() {
 
     const handleUpdateQuantidadeSubmit = async (id, novaQuantidade) => {
         await updateVendedorQuantidade(id, novaQuantidade);
-        await loadVendedores();
         setUpdateModalOpen(false);
-    };    
+    };
 
     const handleShowCompradores = (vendedor) => {
         setVendedorToShowCompradores(vendedor);
@@ -72,9 +71,12 @@ function HomePage() {
         });
     };
 
-    const filteredVendedores = vendedores.filter((vendedor) =>
-        vendedor.nome.toLowerCase().includes(searchQuery.toLowerCase())
-    );
+    // Filtragem e ordenação
+    const filteredVendedores = vendedores
+        .filter((vendedor) =>
+            vendedor.nome.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+        .sort((a, b) => b.quantidade - a.quantidade); // Ordena por quantidade de vendas
 
     const congregacaoRanking = vendedores.reduce((acc, vendedor) => {
         acc[vendedor.congregacao] = (acc[vendedor.congregacao] || 0) + vendedor.quantidade;
@@ -93,99 +95,85 @@ function HomePage() {
     };
 
     return (
-      <Container maxWidth="sm" sx={{ padding: 2 }}>
-        <SearchBar onSearch={setSearchQuery} />
-        <Box
-          sx={{ display: "flex", justifyContent: "space-around", marginY: 2 }}
-        >
-          {congregacaoRankingSorted.map(([congregacao, total], index) => (
-            <Box key={congregacao} sx={{ textAlign: "center" }}>
-              <EmojiEventsIcon
-                style={{ color: getTrophyColor(index), fontSize: "2rem" }}
-              />
-              <Typography variant="h6">
-                {index + 1}° {congregacao}
-              </Typography>
-              <Typography variant="subtitle1">{total} vendas</Typography>
+        <Container maxWidth="sm" sx={{ padding: 2 }}>
+            <SearchBar onSearch={setSearchQuery} />
+            <Box sx={{ display: 'flex', justifyContent: 'space-around', marginY: 2 }}>
+                {congregacaoRankingSorted.map(([congregacao, total], index) => (
+                    <Box key={congregacao} sx={{ textAlign: 'center' }}>
+                        <EmojiEventsIcon style={{ color: getTrophyColor(index), fontSize: '2rem' }} />
+                        <Typography variant="h6">
+                            {index + 1}° {congregacao}
+                        </Typography>
+                        <Typography variant="subtitle1">
+                            {total} vendas
+                        </Typography>
+                    </Box>
+                ))}
             </Box>
-          ))}
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            marginBottom: 2,
-          }}
-        >
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleAddVendedor}
-          >
-            Adicionar Vendedor
-          </Button>
-          <IconButton
-            onClick={handleCopyLink}
-            color="primary"
-            aria-label="compartilhar link"
-          >
-            <ShareIcon />
-          </IconButton>
-        </Box>
-        <VendedorList
-          vendedores={filteredVendedores}
-          onUpdateQuantidade={handleUpdateQuantidade}
-          onDelete={handleConfirmDeleteVendedor} // Chama o modal de confirmação
-          onShowCompradores={handleShowCompradores}
-        />
-        <AddVendedorModal
-          open={isAddModalOpen}
-          onClose={() => setAddModalOpen(false)}
-          onSubmit={handleAddVendedorSubmit}
-        />
-        <UpdateQuantidadeModal
-          open={isUpdateModalOpen}
-          onClose={() => setUpdateModalOpen(false)}
-          vendedor={vendedorToUpdate}
-          onSubmit={handleUpdateQuantidadeSubmit} // Passa a função de submit correta
-        />
-        <CompradoresModal
-          open={isCompradoresModalOpen}
-          onClose={() => setCompradoresModalOpen(false)}
-          vendedor={vendedorToShowCompradores}
-        />
-        <Dialog
-          open={isConfirmDialogOpen}
-          onClose={() => setConfirmDialogOpen(false)}
-        >
-          <DialogTitle>Confirmar Exclusão</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Tem certeza que deseja excluir este vendedor? Esta ação não pode
-              ser desfeita.
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={() => setConfirmDialogOpen(false)} color="primary">
-              Cancelar
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: 2 }}>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleAddVendedor}
+                >
+                    Adicionar Vendedor
+                </Button>
+                <IconButton onClick={handleCopyLink} color="primary" aria-label="compartilhar link">
+                    <ShareIcon />
+                </IconButton>
+            </Box>
+            <VendedorList 
+                vendedores={filteredVendedores}
+                onUpdateQuantidade={handleUpdateQuantidade} 
+                onDelete={handleConfirmDeleteVendedor} // Chama o modal de confirmação
+                onShowCompradores={handleShowCompradores}
+            />
+            <AddVendedorModal 
+                open={isAddModalOpen} 
+                onClose={() => setAddModalOpen(false)} 
+                onSubmit={handleAddVendedorSubmit}
+            />
+            <UpdateQuantidadeModal 
+                open={isUpdateModalOpen} 
+                onClose={() => setUpdateModalOpen(false)} 
+                vendedor={vendedorToUpdate} 
+                onSubmit={handleUpdateQuantidadeSubmit}
+            />
+            <CompradoresModal
+                open={isCompradoresModalOpen}
+                onClose={() => setCompradoresModalOpen(false)}
+                vendedor={vendedorToShowCompradores}
+            />
+            <Dialog
+                open={isConfirmDialogOpen}
+                onClose={() => setConfirmDialogOpen(false)}
+            >
+                <DialogTitle>Confirmar Exclusão</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Tem certeza que deseja excluir este vendedor? Esta ação não pode ser desfeita.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setConfirmDialogOpen(false)} color="primary">
+                        Cancelar
+                    </Button>
+                    <Button onClick={handleDeleteVendedor} color="secondary">
+                        Excluir
+                    </Button>
+                </DialogActions>
+            </Dialog>
+            <Button
+                variant="outlined"
+                color="secondary"
+                fullWidth
+                sx={{ marginTop: 2 }}
+                onClick={logout}
+            >
+                Sair
             </Button>
-            <Button onClick={handleDeleteVendedor} color="secondary">
-              Excluir
-            </Button>
-          </DialogActions>
-        </Dialog>
-        <Button
-          variant="outlined"
-          color="secondary"
-          fullWidth
-          sx={{ marginTop: 2 }}
-          onClick={logout}
-        >
-          Sair
-        </Button>
-        <Signature /> {/* Adiciona a assinatura fixada */}
-      </Container>
+            <Signature /> {/* Adiciona a assinatura fixada */}
+        </Container>
     );
 }
 

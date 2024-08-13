a5e2429441595f7da1f73b1611ee8a9de7b9b6e2
lucas.ribeiro <lucas.ribeiro@cebraspe.org.br>
2024-08-13 17:01:48 -0300

    feat: Atualizar modal de compradores para adicionar rolagem e limitar altura
    Atualiza o componente CompradoresModal.js para adicionar uma barra de rolagem e limitar a altura da lista de compradores exibida. Isso melhora a experiência do usuário ao visualizar uma lista longa de compradores.

--- src/components/CompradoresModal.js
+++ src/components/CompradoresModal.js
@@ -5,9 +5,12 @@ import '../styles/styles.css';
 function CompradoresModal({ open, onClose, vendedor }) {
     return (
         <Modal open={open} onClose={onClose}>
-            <Box className="modal-content">
+            <Box className="modal-content" sx={{ maxWidth: 400, maxHeight: 500, overflow: 'hidden' }}>
                 <Typography variant="h6" className="modal-header">{vendedor?.nome} - Compradores</Typography>
-                <List className="modal-body">
+                <List 
+                    className="modal-body" 
+                    sx={{ maxHeight: 300, overflowY: 'auto', marginBottom: 2 }}
+                >
                     {vendedor?.compradores.map((comprador, idx) => (
                         <ListItem key={idx}>
                             <ListItemText
